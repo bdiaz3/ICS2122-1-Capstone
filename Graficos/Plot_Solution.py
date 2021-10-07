c1527eac8628a5e2909a431ff56c0ec9aced2eaa
tago470 <smorales1@uc.cl>
2021-10-06 23:44:12 -0300

    plot solucion mejorado

--- Graficos/Plot_Solution.py
+++ Graficos/Plot_Solution.py
@@ -10,6 +10,12 @@ COORDENADA_X_BASES = []
 COORDENADA_Y_BASES = []
 COORDENADA_X_EVENTOS = []
 COORDENADA_Y_EVENTOS = []
+GLOBAL = 0.2
+ZOOM = 1
+SIZE = GLOBAL
+SOLUCION_BASE_PRIMER_MODELO = [1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1,1,1,0,0]
+SOLUCION_BASE_SEGUNDO_MODELO = [1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,0,0]
+SOLUCION_BASE = SOLUCION_BASE_SEGUNDO_MODELO
 
 with open('Datos/nodos.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
@@ -45,7 +51,7 @@ with open('Datos/bases.csv') as csv_file:
             COORDENADA_Y_BASES.append(float(row[1].replace(',','.')))
             line_count += 1
 
-with open('Datos/eventos.csv') as csv_file:
+with open('Datos/eventos_excel.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
     for row in csv_reader:
@@ -56,7 +62,7 @@ with open('Datos/eventos.csv') as csv_file:
             COORDENADA_Y_EVENTOS.append(float(row[1].replace(',','.')))
             line_count += 1
 
-SOLUCION_BASE = [1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1,1,1,0,0] #Soluciones aquí
+
 COORDENADA_X_BASES_SOLUCION = []
 COORDENADA_Y_BASES_SOLUCION = []
 for i in range(len(COORDENADA_X_BASES)):
@@ -65,12 +71,12 @@ for i in range(len(COORDENADA_X_BASES)):
         COORDENADA_Y_BASES_SOLUCION.append(COORDENADA_Y_BASES[i])
 
     
-plt.scatter(COORDENADA_X_NODOS, COORDENADA_Y_NODOS, s=0.5)
-#plt.scatter(COORDENADA_X_EVENTOS, COORDENADA_Y_EVENTOS, s=0.1, c = 'orange') # Aca pa plotear eventos
-#plt.scatter(COORDENADA_X_CENTROS, COORDENADA_Y_CENTROS, c = 'red', s=9)
-plt.scatter(COORDENADA_X_BASES, COORDENADA_Y_BASES, c = 'green', s=9)
-plt.scatter(COORDENADA_X_BASES_SOLUCION, COORDENADA_Y_BASES_SOLUCION, c = 'blue', s=9)
-plt.legend(['Nodos','Bases Desocupadas', 'Bases Solución']) #Leyenda grafico: para eventos usar ['Nodos','Eventos','Centros', 'Bases Desocupadas', 'Bases Solución']
+#plt.scatter(COORDENADA_X_NODOS, COORDENADA_Y_NODOS, s=SIZE, zorder=0.5)
+#plt.scatter(COORDENADA_X_EVENTOS, COORDENADA_Y_EVENTOS, s=SIZE, c = 'orange') # Aca pa plotear eventos
+#plt.scatter(COORDENADA_X_CENTROS, COORDENADA_Y_CENTROS, c = 'red', s=15*SIZE, zorder=2)
+plt.scatter(COORDENADA_X_BASES, COORDENADA_Y_BASES, c = 'green', s=15*SIZE, zorder=2)
+plt.scatter(COORDENADA_X_BASES_SOLUCION, COORDENADA_Y_BASES_SOLUCION, c = 'blue', s=15*SIZE, zorder=3)
+plt.legend(['Bases Desocupadas', 'Bases Solución']) #Leyenda grafico: para eventos usar ['Nodos','Eventos','Centros', 'Bases Desocupadas', 'Bases Solución']
 
 with open('Datos/arcos_coordenados.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -83,7 +89,7 @@ with open('Datos/arcos_coordenados.csv') as csv_file:
             p1y = float(row[1])
             p2x = float(row[2])
             p2y = float(row[3])
-            #plt.plot([p1x, p2x],[p1y, p2y], c="lightseagreen", linewidth=0.1)
+            plt.plot([p1x, p2x],[p1y, p2y], c="lightseagreen", linewidth=0.5*SIZE, zorder=1)
             line_count += 1
 ax = plt.axes()
 ax.set_facecolor("black")
